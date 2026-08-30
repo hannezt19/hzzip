5f5c3c51428d0b2e569feb9f4b558aa2f67dd9cf
hannezt19 <Hannezt19@gmail.com>
2026-08-30 19:36:52 +0700

    Fix compose dependency version conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         applicationId = "com.yohanes.filereader"
-        // Android 9 (Redmi C2) sampai versi terbaru (Poco M4)
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -47,11 +46,14 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.1")
-    implementation("androidx.compose.ui:ui:1.6.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
-    implementation("androidx.compose.material3:material3:1.2.1")
+
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.documentfile:documentfile:1.0.1")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
